f176f4f04a175b93dbf0e018e423d6cfa4e37108
b0kchi <bokchi.o33@gmail.com>
2024-08-30 11:32:45 +0900

    style: Publishing based on state values

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,26 +1,28 @@
-import React, { ReactNode } from 'react';
-import styled, { css } from "styled-components";
-import '../styles/button.css';
-import {createColorToken}  from "../tokens/colors";
-import baseColorToken from '../tokens/tokens.json';
-import clsx from 'clsx';
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+import "../styles/button.css";
+import { createColorToken } from "../tokens/colors";
+import baseColorToken from "../tokens/tokens.json";
 
 interface IButtonProps {
-  type?: 'primary' | 'default' | 'link'
+  type?: "primary" | "default" | "link";
   icononly?: boolean | string;
-  size?: 'small' | 'medium' | 'large';
+  size?: "small" | "medium" | "large";
   children?: ReactNode;
   onClick?: () => void;
   disabled?: boolean;
   classNames?: string;
-  icon?: ReactNode | any;
+  icon?: ReactNode | unknown;
 }
 
-let colorChip = createColorToken(); 
-let baseColorChip = baseColorToken;
+const colorChip = createColorToken();
+const baseColorChip = baseColorToken;
+
+console.log("colorChip: ", colorChip);
+console.log("baseColorChip: ", baseColorChip);
 
 const StyledButton = styled.button<IButtonProps>`
-  cursor:pointer;
+  cursor: pointer;
   display: flex;
   align-items: center;
   height: 32px;
@@ -30,58 +32,91 @@ const StyledButton = styled.button<IButtonProps>`
   border-color: ${baseColorChip.colors.border.colorBorder.$value};
   border-radius: 5px;
   gap: 8px;
-  line-height:1;
+  line-height: 1;
   justify-content: space-between;
   padding: 0 16px;
-  img, svg {
+  img,
+  svg {
     width: 16px;
+  }
+
+  &:hover {
+    border-color: ${colorChip.colorPrimaryBgHover};
+    background-color: ${baseColorChip.colors.background.colorBgBase};
+    color: ${colorChip.colorPrimaryTextHover};
   }
 
   &[data-icononly="true"] {
     gap: 0;
-    padding: 0;
+    padding: 8px;
     justify-content: center;
-  };
+    &[data-size="small"] {
+      padding: 4px;
+    }
+    &[data-size="large"] {
+      padding: 11px;
+      svg {
+        width: 18px;
+      }
+    }
+  }
 
   &[data-size="large"] {
     height: 40px;
     font-size: 1rem;
-    img, svg {
+    img,
+    svg {
       width: 18px;
     }
-  };
+  }
   &[data-size="small"] {
     height: 26px;
     padding: 0 12px;
-    img, svg {
+    img,
+    svg {
       width: 14px;
     }
-  };
+  }
 
   &[data-type="primary"] {
     background: #aedf;
     border-color: ${colorChip.colorPrimary};
     background-color: ${colorChip.colorPrimary};
     color: ${baseColorChip.colors.text.colorTextLight.$value};
-  };
+    &:hover {
+      border-color: ${colorChip.colorPrimaryBgHover};
+      background-color: ${colorChip.colorPrimaryBgHover};
+      color: ${colorChip.colorPrimaryTextActive};
+    }
+  }
   &[data-type="link"] {
-    border-color:transparent;
+    border-color: transparent;
     background-color: transparent;
     color: ${colorChip.colorPrimary};
-  };
+    &:hover {
+      color: ${colorChip.colorPrimaryTextHover};
+    }
+  }
+
+  &:disabled,
+  &:disabled:hover {
+    cursor: default;
+    border-color: #d9d9d9;
+    background-color: #f5f5f5;
+    color: ${baseColorChip.colors.text.colorTextQuarternary.$value};
+  }
 `;
 
 export default function Button({
-  size = 'medium',
+  size = "medium",
   icononly = false,
-  type = 'default',
+  type = "default",
   children,
   onClick,
   disabled = false,
   classNames,
-  icon
+  icon,
 }: IButtonProps) {
-
   return (
     <StyledButton
       data-type={type}
@@ -92,14 +127,13 @@ export default function Button({
       className={classNames}
     >
       {icononly ? (
-          <>{icon}</>
-        ): (
-          <>
-            {icon}
-            <p className='text'>{children}</p>
-          </>
-        )
-      }
+        <>{icon}</>
+      ) : (
+        <>
+          {icon}
+          <p className="text">{children}</p>
+        </>
+      )}
     </StyledButton>
   );
-};
+}
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,30 +1,50 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
 
-import Button from '../components/Button';
-import testIcon from '../assets/testIcon.svg';
+import Button from "../components/Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {              // 해당 컴포넌트가 받는 props를 정의한다. 
-  title: 'General/Button',  // 경로를 의미한다. 
-  component: Button,        // 스토리로 다룰 컴포넌트이다.
+const meta = {
+  // 해당 컴포넌트가 받는 props를 정의한다.
+  title: "General/Button", // 경로를 의미한다.
+  component: Button, // 스토리로 다룰 컴포넌트이다.
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    type: {control: 'select', description: '버튼의 종류', defaultValue: 'default'},
-    size: { control: 'select' , description: '버튼의 사이즈', defaultValue: 'medium'},
-    children: { control: 'text' , description: '버튼 내 텍스트', defaultValue: 'button'},
-    disabled: {control: 'boolean', description: '버튼 비활성화 여부', defaultValue: false},
-    icononly: {control: 'boolean', description: '아이콘 버튼 여부', defaultValue: false},
-    icon: {description: '아이콘'},
+    type: {
+      control: "select",
+      description: "버튼의 종류",
+      defaultValue: "default",
+    },
+    size: {
+      control: "select",
+      description: "버튼의 사이즈",
+      defaultValue: "medium",
+    },
+    children: {
+      control: "text",
+      description: "버튼 내 텍스트",
+      defaultValue: "button",
+    },
+    disabled: {
+      control: "boolean",
+      description: "버튼 비활성화 여부",
+      defaultValue: false,
+    },
+    icononly: {
+      control: "boolean",
+      description: "아이콘 버튼 여부",
+      defaultValue: false,
+    },
+    icon: { description: "아이콘" },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn()},
+  args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -33,11 +53,41 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const DefaultButton: Story = {
   args: {
-    children: 'Button',
-    type: 'default',
-    size: 'medium',
+    children: "Delete",
+    type: "default",
+    size: "medium",
     disabled: false,
     icononly: false,
-    icon: <img src={testIcon}></img>
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="ionicon"
+        viewBox="0 0 512 512"
+      >
+        <path
+          d="M112 112l20 320c.95 18.49 14.4 32 32 32h184c17.67 0 30.87-13.51 32-32l20-320"
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="32"
+        />
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-miterlimit="10"
+          stroke-width="32"
+          d="M80 112h352"
+        />
+        <path
+          d="M192 112V72h0a23.93 23.93 0 0124-24h80a23.93 23.93 0 0124 24h0v40M256 176v224M184 176l8 224M328 176l-8 224"
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="32"
+        />
+      </svg>
+    ),
   },
 };
