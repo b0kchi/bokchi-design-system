fb9decce0587a14e5196118d89416bc90618a450
b0kchi <bokchi.o33@gmail.com>
2024-09-03 15:58:41 +0900

    feat: Develop selet all function

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -8,10 +8,15 @@ interface ICheckboxItem {
   label: string;
 }
 
+interface ICheckboxProps {
+  items: Array<ICheckboxItem>;
+  handleAll?: boolean;
+}
+
 const StyledInput = styled.input``;
 const StyledLabel = styled.label``;
 
-const Checkbox = ({ items }: any) => {
+const Checkbox = ({ items, handleAll }: ICheckboxProps) => {
   const [checkedList, setCheckedList] = useState<string[]>([]);
 
   const checkedItemHandler = async (value: string, isChecked: boolean) => {
@@ -26,6 +31,16 @@ const Checkbox = ({ items }: any) => {
     }
   };
 
+  const handleAllCheck = (checked: boolean) => {
+    if (checked) {
+      const idArray: Array<string> = [];
+      items.forEach((elem) => idArray.push(elem.id));
+      setCheckedList(idArray);
+    } else {
+      setCheckedList([]);
+    }
+  };
+
   const checkHandler = (
     e: React.ChangeEvent<HTMLInputElement>,
     value: string
@@ -35,6 +50,17 @@ const Checkbox = ({ items }: any) => {
 
   return (
     <>
+      {handleAll && (
+        <div className="checkbox">
+          <StyledInput
+            type="checkbox"
+            id="select-all"
+            checked={checkedList.length === items.length}
+            onChange={(e) => handleAllCheck(e.target.checked)}
+          />
+          <StyledLabel htmlFor="select-all">전체선택</StyledLabel>
+        </div>
+      )}
       {items.map((item: ICheckboxItem, idx: number) => (
         <div className="checkbox" key={idx}>
           <StyledInput
